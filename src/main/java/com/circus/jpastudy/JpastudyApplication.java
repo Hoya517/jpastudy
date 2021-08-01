3a2cda4add5a9fe865d1cc2d9d04a5f8d0d26616
Kim Juho <sv05159@naver.com>
2021-08-01 17:19:36 +0900

    0801 study end

--- src/main/java/com/circus/jpastudy/JpastudyApplication.java
+++ src/main/java/com/circus/jpastudy/JpastudyApplication.java
@@ -2,7 +2,10 @@ package com.circus.jpastudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //JPA Auditing
 @SpringBootApplication
 public class JpastudyApplication {
 
--- src/main/java/com/circus/jpastudy/domain/Member.java
+++ src/main/java/com/circus/jpastudy/domain/Member.java
@@ -1,16 +1,30 @@
 package com.circus.jpastudy.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+//    @Column(nullable = false, length = 9, columnDefinition = "TEXT")
     private String name;
+
+//    @Column(nullable = true)
     private int age;
+
+    @Builder
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
